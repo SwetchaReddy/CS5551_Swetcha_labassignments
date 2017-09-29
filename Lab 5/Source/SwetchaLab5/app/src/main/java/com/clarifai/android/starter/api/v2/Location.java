a0ebedd5e02e8ebf96964baf4f2cfb2a4b52aff1
SwetchaReddy <31418996+SwetchaReddy@users.noreply.github.com>
2017-09-28 23:29:59 -0500

    android
    android

---
+++ Lab 5/Source/SwetchaLab5/app/src/main/java/com/clarifai/android/starter/api/v2/Location.java
@@ -0,0 +1,37 @@
+package com.clarifai.android.starter.api.v2;
+
+import android.widget.Adapter;
+import android.widget.AdapterView;
+
+import com.clarifai.android.starter.api.v2.activity.RecognizeConceptsActivity;
+
+/**
+ * Created by HP 1 on 28-Sep-17.
+ */
+public final static String ID_EXTRA="com.clarifai.android.starter.api.v2.activity.PICK_IMAGE";
+public class Location {
+
+    public void walmart() {
+        char i;
+        String city[10]='Kansas';
+        public void startActivity(String);
+        {
+            throw new UnsupportedOperationException("No list parameter selected");
+        }
+
+
+
+
+        throw new UnsupportedOperationException("No parameter selected");
+        private AdapterView.OnItemClickListener R.id.resultsList.ID = new AdapterView.OnItemClickListener() {
+            public void OnresultsListClick(AdapterView<?> parent, View activity_recognize.xml, int position, long id) {
+                i.putExtra(RecognizeConceptsActivity.PICK_IMAGE, RecognizeConceptsActivity.ClarifyResponse.list.valueOf(id));
+                startActivity('http://api.walmartlabs.com/v1/stores?apiKey=fdy6abrnhrts7b2zt8cwnhds&lon=-97.5786&lat=39.0997&format=xml');
+            }
+
+        }
+
+    }
+
+}
+
