f632c228d69b9e880583aedbe59cc48620ee9aa2
SwetchaReddy <31418996+SwetchaReddy@users.noreply.github.com>
2017-11-28 20:27:47 -0600

    Update ExampleInstrumentedTest.java

--- Lab 10/Source/AndroidWearWatch/app/src/androidTest/java/com/Lab10/SwetchaReddy/Watch/ExampleInstrumentedTest.java
+++ Lab 10/Source/AndroidWearWatch/app/src/androidTest/java/com/Lab10/SwetchaReddy/Watch/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.aselabassignment.pranoop.lab10androidwear", appContext.getPackageName());
+        assertEquals("com.Lab10.SwetchaReddy.Watch", appContext.getPackageName());
     }
 }
